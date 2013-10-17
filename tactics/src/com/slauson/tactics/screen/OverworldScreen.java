2db85e1925a98b8949ac2bded79f5c850063487d
Josh Slauson <josh@joshslauson.com>
2013-10-16 21:10:11 -0700

    Refactor camera/scoll/zoom handling.

--- tactics/src/com/slauson/tactics/screen/OverworldScreen.java
+++ tactics/src/com/slauson/tactics/screen/OverworldScreen.java
@@ -1,9 +1,12 @@
 package com.slauson.tactics.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
 import com.slauson.tactics.controller.OverworldController;
 import com.slauson.tactics.view.OverworldRenderer;
 
@@ -14,11 +17,28 @@ import com.slauson.tactics.view.OverworldRenderer;
  */
 public class OverworldScreen implements Screen, InputProcessor {
 
+	private static final float PIXELS_PER_METER = 32;
+	
+	private static final float ZOOM_INCREMENT = 0.02f;
+
 	private OverworldRenderer renderer;
 	private OverworldController controller;
 	
+	private int width, height;
+	
+	private OrthographicCamera camera;
+	private int mouseButtonDown;
+	private float zoomAmount;
+	
+	private final Vector3 lastMousePressWorldPosition;
+	private final Vector3 lastMouseScreenPosition;
+	
 	public OverworldScreen() {
+		camera = new OrthographicCamera();
+		lastMousePressWorldPosition = new Vector3();
+		lastMouseScreenPosition = new Vector3();
 		
+		zoomAmount = 1f;
 	}
 	
 	@Override
@@ -27,12 +47,25 @@ public class OverworldScreen implements Screen, InputProcessor {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		controller.update(delta);
-		renderer.render();
+		renderer.render(camera);
 	}
 	
 	@Override
 	public void resize(int width, int height) {
-		renderer.resize(width, height);
+		this.width = width;
+		this.height = height;
+		
+		System.out.println("resize: " + width + ", " + height);
+		
+		// save old position as setToOrtho resets position
+		Vector3 oldCameraPosition = new Vector3(camera.position);
+
+		// scale visible world coordinates based on screen coordinates
+		camera.setToOrtho(false, width/PIXELS_PER_METER, height/PIXELS_PER_METER);
+		
+		// move camera back to its original position
+		camera.translate(oldCameraPosition.x - camera.position.x, oldCameraPosition.y - camera.position.y);
+		camera.update();
 	}
 
 	@Override
@@ -76,25 +109,50 @@ public class OverworldScreen implements Screen, InputProcessor {
 
 	@Override
 	public boolean keyTyped(char character) {
-		renderer.keyTyped(character);
+		switch (character) {
+		case 'z':
+			zoomAmount += ZOOM_INCREMENT;
+			camera.zoom = zoomAmount;
+			camera.update();
+			break;
+		case 'x':
+			zoomAmount -= ZOOM_INCREMENT;
+			camera.zoom = zoomAmount;
+			camera.update();
+			break;
+		}
 		return true;
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		renderer.touchDown(screenX, screenY, pointer, button);		
+		System.out.println("touchDown: " + screenX + ", " + screenY + ", " + pointer + ", " + button);
+		
+		lastMousePressWorldPosition.set(screenX, screenY, 0);
+		camera.unproject(lastMousePressWorldPosition);
+		mouseButtonDown = button;
+		
+		// set this here so we use the updated position in touchDragged
+		lastMouseScreenPosition.set(screenX, screenY, 0);
 		return true;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		renderer.touchUp(screenX, screenY, pointer, button);
+		mouseButtonDown = 0;
 		return true;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		renderer.touchDragged(screenX, screenY, pointer);
+		System.out.println("touchDragged: " + screenX + ", " + screenY + ", " + pointer);
+		
+		if (mouseButtonDown == Input.Buttons.RIGHT) {
+			camera.translate((lastMouseScreenPosition.x - screenX)/PIXELS_PER_METER, (screenY - lastMouseScreenPosition.y)/PIXELS_PER_METER);
+			camera.update();
+		}
+		
+		lastMouseScreenPosition.set(screenX, screenY, 0);
 		return true;
 	}
 
--- tactics/src/com/slauson/tactics/view/OverworldRenderer.java
+++ tactics/src/com/slauson/tactics/view/OverworldRenderer.java
@@ -1,11 +1,9 @@
 package com.slauson.tactics.view;
 
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Vector3;
 
 /**
  * Renders the overworld.
@@ -15,27 +13,12 @@ import com.badlogic.gdx.math.Vector3;
 public class OverworldRenderer {
 	private static final int GRID_SIZE = 10;
 	
-	private static final float PIXELS_PER_METER = 32;
-	
-	private static final float ZOOM_INCREMENT = 0.02f;
-
-	private OrthographicCamera camera;
-	
-	private ShapeRenderer renderer = new ShapeRenderer();
+	private ShapeRenderer renderer;
 	
 	private int width, height;
 	
-	private final Vector3 lastMousePressWorldPosition;
-	private final Vector3 lastMouseScreenPosition;
-	
-	private int mouseButtonDown;
-	private float zoomAmount = 1f;
-	
-	
 	public OverworldRenderer() {
-		camera = new OrthographicCamera();
-		lastMousePressWorldPosition = new Vector3();
-		lastMouseScreenPosition = new Vector3();
+		renderer = new ShapeRenderer();
 	}
 	
 	/**
@@ -44,22 +27,6 @@ public class OverworldRenderer {
 	 * @param height
 	 */
 	public void resize(int width, int height) {
-		
-		this.width = width;
-		this.height = height;
-		
-		System.out.println("resize: " + width + ", " + height);
-		
-		// save old position as setToOrtho resets position
-		Vector3 oldCameraPosition = new Vector3(camera.position);
-
-		// scale visible world coordinates based on screen coordinates
-		camera.setToOrtho(false, width/PIXELS_PER_METER, height/PIXELS_PER_METER);
-		
-		// move camera back to its original position
-		camera.translate(oldCameraPosition.x - camera.position.x, oldCameraPosition.y - camera.position.y);
-		camera.update();
-		
 		this.width = width;
 		this.height = height;
 	}
@@ -67,7 +34,7 @@ public class OverworldRenderer {
 	/**
 	 * Renders the overworld.
 	 */
-	public void render() {
+	public void render(OrthographicCamera camera) {
 		renderer.setProjectionMatrix(camera.combined);
 		renderer.begin(ShapeType.Rectangle);
 		for (int i = 0; i < GRID_SIZE; i++) {
@@ -78,72 +45,4 @@ public class OverworldRenderer {
 		}
 		renderer.end();
 	}
-	
-	/**
-	 * Handles key type events.
-	 * @param character
-	 */
-	public void keyTyped(char character) {
-		switch (character) {
-		case 'z':
-			zoomAmount += ZOOM_INCREMENT;
-			camera.zoom = zoomAmount;
-			camera.update();
-			break;
-		case 'x':
-			zoomAmount -= ZOOM_INCREMENT;
-			camera.zoom = zoomAmount;
-			camera.update();
-			break;
-		}
-	}
-	
-	/**
-	 * Handles touch down events.
-	 * @param screenX
-	 * @param screenY
-	 * @param pointer
-	 * @param button
-	 */
-	public void touchDown(int screenX, int screenY, int pointer, int button) {
-		
-		System.out.println("touchDown: " + screenX + ", " + screenY + ", " + pointer + ", " + button);
-		
-		lastMousePressWorldPosition.set(screenX, screenY, 0);
-		camera.unproject(lastMousePressWorldPosition);
-		mouseButtonDown = button;
-		
-		// set this here so we use the updated position in touchDragged
-		lastMouseScreenPosition.set(screenX, screenY, 0);
-	}
-	
-	/**
-	 * Handles touch up events.
-	 * @param screenX
-	 * @param screenY
-	 * @param pointer
-	 * @param button
-	 */
-	public void touchUp(int screenX, int screenY, int pointer, int button) {
-		mouseButtonDown = 0;
-	}
-	
-	/**
-	 * Handles touch drag events.
-	 * @param screenX
-	 * @param screenY
-	 * @param pointer
-	 */
-	public void touchDragged(int screenX, int screenY, int pointer) {
-		
-		System.out.println("touchDragged: " + screenX + ", " + screenY + ", " + pointer);
-		
-		if (mouseButtonDown == Input.Buttons.RIGHT) {
-			camera.translate((lastMouseScreenPosition.x - screenX)/OverworldRenderer.PIXELS_PER_METER, (screenY - lastMouseScreenPosition.y)/OverworldRenderer.PIXELS_PER_METER);
-			camera.update();
-		}
-		
-		lastMouseScreenPosition.set(screenX, screenY, 0);
-	}
-
 }
